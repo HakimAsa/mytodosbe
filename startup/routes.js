dde46686edabe86b502cd0d3b0dbd95af64b0108
HakimAsa <igbo7777@gmail.com>
2023-02-17 20:20:54 +0100

    tuning on helmet issue on prod

--- startup/routes.js
+++ startup/routes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const path = require('path');
-
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
 //relative paths
 const todos = require('../routes/todos');
 const { notFound, error } = require('../middleware/error');
+
+const cors = require('cors');
 
 module.exports = function (app) {
   app.use(cors());
@@ -19,20 +19,19 @@ module.exports = function (app) {
   app.use(mongoSanitize());
   // secure headers
   app.use(helmet({ crossOriginEmbedderPolicy: false }));
+  app.use(express.json({ limit: '200mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 
-  app.use(express.json());
-  app.use(
-    express.urlencoded({
-      extended: true,
-    })
-  );
-  app.use(cookieParser());
-
-  // app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   //   app.use(express.static(path.join(__dirname, '../public')));
   //   app.use('/api/uploads', express.static(path.join(__dirname, '../public/uploads')));
   //   app.use('/api/userImages', express.static('api/userImages')); //todo
+
+  //entry point
+  app.get('/', (req, res) => {
+    res.json({ mytodoapp: 'is awesome' });
+  });
+
   app.use('/api/v1/todos', todos);
 
   //global error middleware
