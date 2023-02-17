f6f1b0c8e7e23d010dc769f6821c4e726f2e6fd3
HakimAsa <igbo7777@gmail.com>
2023-02-17 19:29:25 +0100

    cleaning up

--- server.js
+++ server.js
@@ -12,14 +12,6 @@ require('./startup/prod')(app);
 const port = process.env.PORT || 2023;
 const env = process.env.NODE_ENV || 'development';
 
-app.get('/', (req, res) => {
-  res.send('starting my todos backend api');
-});
-
-// app.listen(port, () => {
-//   console.log(`Todo app api listening on port ${port}`);
-// });
-
 const server = app.listen(port, () => winston.info(`Listening on port ${port} in ${env} mode...`.yellow.underline.bold));
 
 module.exports = server;
