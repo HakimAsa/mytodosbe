0374fff99a2f1e8549ce491c881fb11f35eb8606
aamidou <akimayena26@gmail.com>
2025-03-10 20:59:20 +0100

    adding readme file and fixing wrong import routes

---
+++ readme.md
@@ -0,0 +1,14 @@
+- after cloning the project from GitHub by using <git clone https://github.com/HakimAsa/mytodosbe.git> command:
+
+1.  npm install or yarn install
+2.  SET UP DB URL
+
+    1.1. Local database
+
+    if you have the mongo compass installed, then you set the following string in config/default.json file: "db":"mongodb://localhost:27017
+
+    1.2. Remote database
+
+    If you have to head over https://cloud.mongodb.com/ to sign up or login. Please follow the instructions from mongodb site. create a db named tododb(or whatever you like) and copy the string url "mongodb+srv://<db_username>:<db_password>@cluster0.lxd3q.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=Cluster0".
+
+        open your terminal (please if you're on window, download gitbash) and cd to project root; copy and paste this: export TODOSDB="mongodb+srv://<db_username>:<db_password>@cluster0.lxd3q.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=Cluster0", then hit enter. Please remember to replace your own database credentials.
--- startup/routes.js
+++ startup/routes.js
@@ -1,48 +1,48 @@
-const express = require('express');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
+const express = require('express')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 
 //relative paths
-const todos = require('../routes/todos');
-const { notFound, error } = require('../middleware/error');
-const { glb } = require('../utils/Globals');
-const { ep } = require('../utils/Endpoints');
+const todos = require('../routes/todo.routes')
+const { notFound, error } = require('../middleware/error')
+const { glb } = require('../utils/Globals')
+const { ep } = require('../utils/Endpoints')
 
 module.exports = function (app) {
   //Body Parser
-  app.use(express.json({ limit: '200mb' }));
-  app.use(express.urlencoded({ extended: true, limit: '200mb' }));
+  app.use(express.json({ limit: '200mb' }))
+  app.use(express.urlencoded({ extended: true, limit: '200mb' }))
   //Cookie Parser
-  app.use(cookieParser());
+  app.use(cookieParser())
 
   // Enable CORS
-  app.use(cors());
+  app.use(cors())
 
   //Sanitize data
-  app.use(mongoSanitize());
+  app.use(mongoSanitize())
 
   // Secure headers
-  app.use(helmet({ crossOriginEmbedderPolicy: false }));
+  app.use(helmet({ crossOriginEmbedderPolicy: false }))
 
   // Prevent XSS attacks
-  app.use(xss());
+  app.use(xss())
 
   // Rate Limiting
   const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, //10 min
     max: 50, // 50 requests can be made in 10 min
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  });
-  app.use(limiter);
+  })
+  app.use(limiter)
 
   // Prevent http param polution
-  app.use(hpp());
+  app.use(hpp())
 
   //   app.use(express.static(path.join(__dirname, '../public')));
   //   app.use('/api/uploads', express.static(path.join(__dirname, '../public/uploads')));
@@ -50,17 +50,17 @@ module.exports = function (app) {
 
   //entry point
   app.get('/', (req, res) => {
-    res.json({ mytodoapp: 'is awesome' });
-  });
+    res.json({ mytodoapp: 'is awesome' })
+  })
 
   //favicon for fixing that request on prodcution
   app.get('/favicon.ico', (req, res) => {
-    res.json({ myfavico: 'is coming soon...' });
-  });
+    res.json({ myfavico: 'is coming soon...' })
+  })
 
-  app.use(glb.doSetForwardSlash(ep.API, ep.V1, ep.TODOS), todos);
+  app.use(glb.doSetForwardSlash(ep.API, ep.V1, ep.TODOS), todos)
 
   //global error middleware
-  app.use(notFound);
-  app.use(error);
-};
+  app.use(notFound)
+  app.use(error)
+}
