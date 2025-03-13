1f576f6007143427790ed573f9b7e72dd0d1a127
aamidou <akimayena26@gmail.com>
2025-03-13 17:32:14 +0100

    add user model with Joi validation

--- models/user.models.js
+++ models/user.models.js
@@ -1,28 +1,62 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require('joi')
+const mongoose = require('mongoose')
+const { glb } = require('../utils/Globals')
+const { COL } = require('../utils/Collections')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    index: true,
-    trim: true,
-    sparse: true,
-    minlength: 5,
-    maxlength: 255,
-  },
-  passowrd: {
-    type: String,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      index: true,
+      trim: true,
+      sparse: true,
+      minlength: 5,
+      maxlength: 255,
+    },
+    passowrd: {
+      type: String,
+    },
 
-  isadmin: {
-    type: Boolean,
-    default: false,
+    role: {
+      type: String,
+      enum: ['user', 'admin', 'premium'],
+      default: 'user',
+    },
+    language: {
+      type: String,
+      enum: ['en', 'fr'], //'es', 'de', 'it', 'pt'
+      default: 'en',
+    },
   },
-});
+  {
+    timestamps: true,
+    toJSON: { virtuals: true, setters: true },
+    toObject: { virtuals: true, setters: true },
+  }
+)
+
+const User = mongoose.model(glb.capitalizeFirstLetter(COL.USER), userSchema)
+function validateUser(user, isRequired) {
+  const schema = Joi.object({
+    username: isRequired
+      ? Joi.string().min(5).max(50).required()
+      : Joi.string().min(5).max(50),
+    email: Joi.string().min(5).max(255).email().required(),
+    passowrd: Joi.string().min(5).max(255).required(),
+    role: Joi.string().valid('user', 'admin', 'premium'),
+    language: Joi.string().valid('en', 'fr'), //'es', 'de', 'it', 'pt'
+  })
+
+  return schema.validate(user)
+}
+
+exports.User = User
+exports.validate = validateUser
