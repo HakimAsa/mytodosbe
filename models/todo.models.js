b994b4bc346a42bab04809d7151df39fc29e144f
aamidou <akimayena26@gmail.com>
2025-03-17 10:11:34 +0100

    remove note field in todo schema

--- models/todo.models.js
+++ models/todo.models.js
@@ -1,11 +1,11 @@
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
+const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
+const mongoose = require('mongoose')
 
-const { COL } = require('../utils/Collections');
-const { glb } = require('../utils/Globals');
+const { COL } = require('../utils/Collections')
+const { glb } = require('../utils/Globals')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const todoSchema = new Schema(
   {
@@ -18,10 +18,6 @@ const todoSchema = new Schema(
       type: String,
       enum: ['suspended', 'finish', 'waiting', 'plan', 'new', 'working'],
       default: 'new',
-    },
-    note: {
-      type: String,
-      maxlength: 250,
     },
     enddate: Date,
     startdate: Date,
@@ -37,22 +33,30 @@ const todoSchema = new Schema(
     toJSON: { virtuals: true, setters: true },
     toObject: { virtuals: true, setters: true },
   }
-);
+)
 
-const Todo = mongoose.model(glb.capitalizeFirstLetter(COL.TODO), todoSchema);
+const Todo = mongoose.model(glb.capitalizeFirstLetter(COL.TODO), todoSchema)
 
 function validateTodo(todo, isRequired = true) {
   const schema = Joi.object({
-    text: isRequired ? Joi.string().min(5).max(2000).required() : Joi.string().min(5).max(2000),
-    note: Joi.string().min(2).max(2000),
-    status: Joi.string().valid('suspended', 'finish', 'waiting', 'plan', 'new', 'working'),
+    text: isRequired
+      ? Joi.string().min(5).max(2000).required()
+      : Joi.string().min(5).max(2000),
+    status: Joi.string().valid(
+      'suspended',
+      'finish',
+      'waiting',
+      'plan',
+      'new',
+      'working'
+    ),
     startdate: Joi.date(),
     enddate: Joi.date(),
     duration: Joi.string(),
-  });
+  })
 
-  return schema.validate(todo);
+  return schema.validate(todo)
 }
 
-exports.Todo = Todo;
-exports.validate = validateTodo;
+exports.Todo = Todo
+exports.validate = validateTodo
